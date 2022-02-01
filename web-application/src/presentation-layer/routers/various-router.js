b76ce6f626a0c606f15aefa0ec392a48bbb68acc
FutureEngineering <daniel_p@LAPTOP-6EUQLR62.localdomain>
2022-02-01 15:17:53 +0100

    changed location of layouts

---
+++ web-application/src/presentation-layer/routers/various-router.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get("/", function(request, response){
+	response.render("start.hbs")
+})
+
+router.get("/about", function(request, response){
+	response.render("about.hbs")
+})
+
+router.get("/contact", function(request, response){
+	response.render("contact.hbs")
+})
+
+module.exports = router
