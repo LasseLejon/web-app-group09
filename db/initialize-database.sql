f3b6749fc3ea486a719b2fd7b9b84e524bae0526
Lasse Lejon <lasse.lejon@hotmail.com>
2022-02-01 15:42:14 +0100

    Implemented initialize-database.sql file

--- db/initialize-database.sql
+++ db/initialize-database.sql
@@ -1,4 +1,21 @@
-CREATE TABLE humans(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(20)
-)
+CREATE TABLE Accounts(
+    accountId INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(10) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+	CONSTRAINT usernameUnique UNIQUE (username)
+);
+
+CREATE TABLE Scanners(
+    scannerId INT AUTO_INCREMENT PRIMARY KEY,
+    scannerNumber INT
+);
+
+CREATE TABLE ScannerBorrowSession(
+    scannerBorrowSessionId INT AUTO_INCREMENT PRIMARY KEY,
+    borrowDate DATETIME NOT NULL,
+    returnDate DATETIME,
+    accountId INT,
+    scannerId INT,
+    FOREIGN KEY (scannerId) REFERENCES Scanners(scannerId),
+    FOREIGN KEY (accountId) REFERENCES Accounts(accountId)
+);
