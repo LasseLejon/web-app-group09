55df75e3adf8c5167578bf085fb0b07afdddc4a3
Lasse Lejon <lasse.lejon@hotmail.com>
2022-02-28 14:40:24 +0100

    Added a default admin user that is created with the database

--- db/initialize-database.sql
+++ db/initialize-database.sql
@@ -20,3 +20,5 @@ CREATE TABLE ScannerBorrowSession(
     FOREIGN KEY (scannerId) REFERENCES Scanners(scannerId),
     FOREIGN KEY (accountId) REFERENCES Accounts(accountId)
 );
+
+INSERT INTO Accounts (username, password, isAdmin) VALUES ('admin', '$2a$10$f/.KuLqLZJTpuBPYq1u2i.tuzrNBxooPrKXkWGeIMhaoCO2C0aQhi', 'yes')
