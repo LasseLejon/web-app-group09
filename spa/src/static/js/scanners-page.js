976ee02028a8ff7ec48b49f9aa4d5f18b3d0a54b
Lasse Lejon <lasse.lejon@hotmail.com>
2022-03-14 19:08:04 +0100

    Changed the scanners to be shown in a table instead of ul

--- spa/src/static/js/scanners-page.js
+++ spa/src/static/js/scanners-page.js
@@ -6,14 +6,38 @@ async function loadScannersPage(){
 	
 	const scanners = await response.json()
 	
-	const allScannersUl = document.getElementById('all-scanners')
-	allScannersUl.innerText = ""
+	const allScannersTable = document.getElementById('all-scanners')
+	allScannersTable.innerText = ""
 
 	
 	for(const scanner of scanners){
-		const li = document.createElement('li')
-		li.innerText = scanner.scannerId
-		allScannersUl.appendChild(li)		
+		const tr = document.createElement('tr')
+		const tdId = document.createElement('td')
+		const tdInUse = document.createElement('td')
+		const tdUpdate = document.createElement('td')
+		const tdDelete = document.createElement('td')
+
+		const anchorUpdate = document.createElement('a')
+		const anchorDelete = document.createElement('a')
+
+		anchorUpdate.href = "scanner/update/" + scanner.scannerId
+		anchorUpdate.innerText = "Update"
+
+		anchorDelete.href = "scanner/delete/" + scanner.scannerId
+		anchorDelete.innerText = "Delete"
+
+		tdId.innerText = scanner.scannerId
+		tdInUse.innerText = scanner.scannerInUse
+
+		tdUpdate.appendChild(anchorUpdate)
+		tdDelete.appendChild(anchorDelete)
+
+
+		tr.appendChild(tdId)
+		tr.appendChild(tdInUse)
+		tr.appendChild(tdUpdate)
+		tr.appendChild(tdDelete)
+		allScannersTable.appendChild(tr)	
 	}
 	
 }
