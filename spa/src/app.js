7bdf4a35580923ad7f95f69250f49758b82a2857
FutureEngineering <daniel_p@LAPTOP-6EUQLR62.localdomain>
2022-03-14 16:53:25 +0100

    merged

--- spa/src/app.js
+++ spa/src/app.js
@@ -5,11 +5,11 @@ const app = express()
 app.use(express.static(__dirname+'/static'))
 
 app.get("*", function(request, response){
-<<<<<<< HEAD
+
 	response.sendFile(__dirname+"/src/js/index.html")
-=======
+
 	response.sendFile(__dirname+"/static/index.html")
->>>>>>> 3bedeaf2d08784c51c3577b5251494e8ba37ce77
+
 })
 
 app.listen(4000)
