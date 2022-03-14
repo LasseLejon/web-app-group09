147fef09279691cde0f68c0894f86ace411cc4ef
FutureEngineering <daniel_p@LAPTOP-6EUQLR62.localdomain>
2022-03-14 14:21:10 +0100

    added packe-lock and package.json

--- web-application/src/business-logic-layer/account-validators.js
+++ web-application/src/business-logic-layer/account-validators.js
@@ -7,6 +7,27 @@ exports.getErrorsNewAccount = function(account){
     console.log("test1",account.isAdmin)
     console.log("test2", account.username)
     console.group("test3", account.password)
+
+    if(!account.hasOwnProperty("username")){
+        errors.push("usernameMissing")
+    }
+    console.log("isadmin",account.isAdmin)
+    if(account.isAdmin != "yes" && account.isAdmin != "no"){
+        errors.push("Please write yes or no on admin status, do not use capital letters")
+    }
+    if(account.username.length < MIN_USERNAME_LENGTH){
+        errors.push("The username needs to be at least "+MIN_USERNAME_LENGTH+" characters.")
+    }
+    if(account.username.length > MAX_USERNAME_LENGTH){
+        errors.push("The username can maximum be "+MAX_USERNAME_LENGTH+" characters.")
+    } 
+	return errors	
+}
+exports.getErrorsNewAccountRestApi = function(account){	
+    const errors = []
+    console.log("test1",account.isAdmin)
+    console.log("test2", account.username)
+    console.group("test3", account.password)
     
     if(!account.hasOwnProperty("username")){
         errors.push("usernameMissing")
