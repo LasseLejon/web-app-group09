45411c0099dbbfa130ac1d5d267544256c0a7612
FutureEngineering <daniel_p@LAPTOP-6EUQLR62.localdomain>
2022-03-26 15:02:26 +0100

    fixed indentation and cleanup for create scanner

--- spa/src/static/js/create-scanner-page.js
+++ spa/src/static/js/create-scanner-page.js
@@ -1,60 +1,45 @@
 async function loadCreateScannerPage(){
-//    const response = await fetch("http://localhost:3000/api/scanner/create")
-//    const scanner = await response.json()
     const ul = document.getElementById('create-error-ul')
     ul.innerText = ""
     const inputValue = document.getElementById('create-scanner-input').value
     inputValue.innerText = ""
 }
+
 async function submitCreateScannerForm(){
     const form = document.getElementById('createScannerForm')
     const ul = document.getElementById('create-error-ul')
     ul.innerText = ""
-
-    
-
-        const inputValue = document.getElementById('create-scanner-input').value
-        console.log("twicesss")
-        console.log("input", inputValue)
-        const errors = getValidationErrorsCreateScannerInput(inputValue)
-        if(errors.length > 0){
-            for(const error of errors){
-                const li = document.createElement('li')
-                li.innerText = error
-                ul.appendChild(li)
-            }
+    const inputValue = document.getElementById('create-scanner-input').value
+    const errors = getValidationErrorsCreateScannerInput(inputValue)
+    if(errors.length > 0){
+        for(const error of errors){
+            const li = document.createElement('li')
+            li.innerText = error
+            ul.appendChild(li)
+        }
+    }
+    else{
+        const response = await fetch("http://localhost:3000/api/scanners", {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer '+ACCESS_TOKEN,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({scannerId : inputValue})   
+        })
+        if(response.status == 200){
+            hideCurrentPage()
+            window.history.pushState(null, "", '/scanner')
+            showPage('/scanner')
         }
         else{
-        const response = await fetch("http://localhost:3000/api/scanners", {
-           method: 'POST',
-           headers: {
-               'Authorization': 'Bearer '+ACCESS_TOKEN,
-               'Content-Type': 'application/json',
-               'Accept': 'application/json'
-           },
-           body: JSON.stringify({scannerId : inputValue})
-       
-        })
-            console.log(response)
-            if(response.status == 200){
-                hideCurrentPage()
-                window.history.pushState(null, "", '/scanner')
-                showPage('/scanner')
-            }
-            else{
-                ul.innerText = ""
-                const li = document.createElement('li')
-                li.innerText = response.statusText
-                ul.appendChild(li)    
-                
-            }
+            ul.innerText = ""
+            const li = document.createElement('li')
+            li.innerText = response.statusText
+            ul.appendChild(li)               
         }
-    //    const data = await response.json()
-    //    console.log("data",data)
-    //   ACCESS_TOKEN = status.access_token
-
-
-
+    }   
 }
 
 function getValidationErrorsCreateScannerInput(input){
